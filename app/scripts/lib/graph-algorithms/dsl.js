07a9d784a658c78645663d237a0ca486ead5392f
Stuart McFarlane <stuart@mcfarlane.nl>
2013-09-19 17:22:36 +0200

    dsl uses strategy

--- app/scripts/lib/graph-algorithms/dsl.js
+++ app/scripts/lib/graph-algorithms/dsl.js
@@ -2,104 +2,102 @@ define(['settings', '../graph-algorithms', 'models/path', 'models/edge', 'lodash
     function (settings, GraphAlgorithms, Path, Edge) {
         'use strict';
 
-        function exhausted(state) {
-            if (state.costAvailable < 0 || isNaN(state.costAvailable)) {
-                settings.debug.dsl && console.log('exhausted: time\'s up', state.start.name);
-                state.pathSoFar.pop();
-                return true;
-            }
-            if (state.pathSoFar.length > 1 && -1 !== state.pathSoFar.indexOf(state.start._id)) {
-                var from = state.pathSoFar[state.pathSoFar.length - 2];
-                var to = state.pathSoFar[state.pathSoFar.length - 1];
-                var count = 0;
-                for (var i = 0; i < state.pathSoFar.length - 2; i++) {
-                    if (state.pathSoFar[i] === from && state.pathSoFar[i+1] === to) {
-                        count++;
-                    }
-                }
-                if (count >= state.maxEdgeRepeats) {
-                    settings.debug.dsl && console.log('exhausted: leg limit reached: ' + from + ' - ' + to);
-                    state.pathSoFar.pop();
+        GraphAlgorithms.prototype.dsl = function dsl(graph, start, end, options) {
+
+            function exhausted(state) {
+                if (state.costAvailable < 0 || isNaN(state.costAvailable)) {
+                    settings.debug.dsl && console.log('exhausted: time\'s up', state.start.name);
                     return true;
                 }
             }
-            return false;
-        }
 
-        function found(state) {
-            if (state.start === state.end) {
-                state.pathSoFar.pop();
-                return true;
-            }
-            return false;
-        }
-
-        function childState(state, child) {
-            var cost = state.costFn({ start: state.start, end: child }, state.costAvailable);
-            var nextState = _.extend({}, state, {
-                start: child,
-                costAvailable: state.costAvailable - cost
-            });
-
-            settings.debug.dsl && console.log('childState: '+
-                state.start.name+' -> '+child.name+' : '+
-                GraphAlgorithms.prototype.edgeLength({start: nextState.start, end: nextState.end})+' m, ' +
-                cost/3600+' hrs');
-            settings.debug.dsl && console.log(' childState: cost', cost/3600, nextState.costAvailable/3600);
-            nextState.pathSoFar.push(child._id);
-            return nextState;
-        }
-
-        GraphAlgorithms.prototype.dsl = function dsl(graph, state) {
-            settings.debug.dsl && console.log('>dsl: ', state);
-            settings.debug.dsl && console.log(' dsl: ' +
-                state.start.name + ' - ' + state.end.name + ' : ' +
-                state.costAvailable/3600 + ' hrs');
-            var paths = [];
-            var that = this;
-
-            if (found(state)) {
-                settings.debug.dsl && console.log('<dsl: found', state.start.name, state.end.name);
-                var path = new Path();
-                path.addVertex(state.start);
-                return [path];
+            function found(state) {
+                if (state.start === state.end) {
+                    return true;
+                }
+                return false;
             }
 
-            if (exhausted(state)) {
-                settings.debug.dsl && console.log('<dsl: exhausted', state.start.name);
-                return undefined;
-            }
-            
-            var children = graph.getChildren(state.start);
-            var allChildPaths = children.map(function childPaths(child) {
-                return that.dsl.call(that, graph, childState(state, child));
-            });
-
-            settings.debug.dsl && console.log('dsl: allChildPaths', state.start.name, state.costAvailable, allChildPaths);
-            allChildPaths = allChildPaths
-            .filter(function filterChildren(childPaths) {
-                // remove dead paths
-                return childPaths !== undefined;
-            });
-            settings.debug.dsl && console.log('dsl: allChildPaths filtered', state.start.name, state.costAvailable, allChildPaths);
-            if (!allChildPaths.length) {
-                settings.debug.dsl && console.log('<dsl: no child paths');
-                return undefined;
-            }
-
-            allChildPaths.forEach(function buildPath(childPaths) {
-                settings.debug.dsl && console.log('buildPath', state.start.name, state.end.name, childPaths);
-                childPaths.forEach(function expandChild(childPath) {
-                    settings.debug.dsl && console.log('expandChildPaths', state.start.name, state.end.name, childPath);
-                    paths.push(childPath.prependVertex(state.start));
+            function childState(state, child) {
+                var cost = 1;
+                var nextState = _.extend({}, state, {
+                    start: child,
+                    costAvailable: state.costAvailable - cost
                 });
-            });
-            settings.debug.dsl && console.log('dsl: paths mapped', state.start.name, state.costAvailable, paths);
 
-            settings.debug.dsl && console.log('<dsl', paths);
-            state.pathSoFar.pop();
-            return paths;
-        };
+                return nextState;
+            }
+
+            function search(graph, state) {
+                settings.debug.dsl && console.log('>dsl: ', state);
+                settings.debug.dsl && console.log(' dsl: ' +
+                    state.start.name + ' - ' + state.end.name + ' : ' +
+                    state.costAvailable/3600 + ' hrs');
+                var paths = [];
+
+                if (options.strategy.found(state)) {
+                    settings.debug.dsl && console.log('<dsl: found', state.start.name, state.end.name);
+                    var path = new Path();
+                    path.addVertex(state.start);
+                    return [path];
+                }
+
+                if (options.strategy.exhausted(state)) {
+                    settings.debug.dsl && console.log('<dsl: exhausted', state.start.name);
+                    return undefined;
+                }
+                
+                var children = graph.getChildren(state.start);
+                var allChildPaths = children.map(function childPaths(child) {
+                    return search(graph, options.strategy.childState(state, child));
+                });
+
+                settings.debug.dsl && console.log('dsl: allChildPaths', state.start.name, state.costAvailable, allChildPaths);
+                allChildPaths = allChildPaths
+                .filter(function filterChildren(childPaths) {
+                    // remove dead paths
+                    return childPaths !== undefined;
+                });
+                settings.debug.dsl && console.log('dsl: allChildPaths filtered', state.start.name, state.costAvailable, allChildPaths);
+                if (!allChildPaths.length) {
+                    settings.debug.dsl && console.log('<dsl: no child paths');
+                    return undefined;
+                }
+
+                allChildPaths.forEach(function buildPath(childPaths) {
+                    settings.debug.dsl && console.log('buildPath', state.start.name, state.end.name, childPaths);
+                    childPaths.forEach(function expandChild(childPath) {
+                        settings.debug.dsl && console.log('expandChildPaths', state.start.name, state.end.name, childPath);
+                        paths.push(childPath.prependVertex(state.start));
+                    });
+                });
+                settings.debug.dsl && console.log('dsl: paths mapped', state.start.name, state.costAvailable, paths);
+
+                settings.debug.dsl && console.log('<dsl', paths);
+                state.pathSoFar.pop();
+                return paths;
+            }
+
+            var defaultOptions = {
+                strategy: {
+                    exhausted: exhausted,
+                    found: found,
+                    childState: childState
+                },
+                state: {
+                    costAvailable: 0
+                }
+            };
+
+            options = _.merge({}, defaultOptions, options);
+            var state = _.extend({}, options.state, {
+                start: start,
+                end: end
+            });
+
+            return search(graph, state);
+
+        }
 
         return GraphAlgorithms;
     }
--- app/scripts/lib/graph-algorithms/paths-with-time.js
+++ app/scripts/lib/graph-algorithms/paths-with-time.js
@@ -2,26 +2,75 @@ define(['settings', 'lib/convert', '../graph-algorithms', 'models/path', 'models
     function (settings, convert, GraphAlgorithms, Path, Edge) {
     'use strict';
 
-    var defaultOptions = {
-        speed: function speed(edge, time) {
-            return convert.knots2mps(6);
-        },
-        maxEdgeRepeats: 2
-    };
-
     GraphAlgorithms.prototype.pathsWithTime = function pathsWithTime(graph, start, end, options) {
+
+        var defaultOptions = {
+            speed: function speed(edge, time) {
+                return convert.knots2mps(6);
+            },
+            maxEdgeRepeats: 2
+        };
+
+        var strategy = {
+            exhausted: function exhausted(state) {
+                if (state.costAvailable < 0 || isNaN(state.costAvailable)) {
+                    settings.debug.dsl && console.log('exhausted: time\'s up', state.start.name);
+                    state.pathSoFar.pop();
+                    return true;
+                }
+                if (state.pathSoFar.length > 1 && -1 !== state.pathSoFar.indexOf(state.start._id)) {
+                    var from = state.pathSoFar[state.pathSoFar.length - 2];
+                    var to = state.pathSoFar[state.pathSoFar.length - 1];
+                    var count = 0;
+                    for (var i = 0; i < state.pathSoFar.length - 2; i++) {
+                        if (state.pathSoFar[i] === from && state.pathSoFar[i+1] === to) {
+                            count++;
+                        }
+                    }
+                    if (count >= state.maxEdgeRepeats) {
+                        settings.debug.dsl && console.log('exhausted: leg limit reached: ' + from + ' - ' + to);
+                        state.pathSoFar.pop();
+                        return true;
+                    }
+                }
+                return false;
+            },
+            found: function found(state) {
+                if (state.start === state.end) {
+                    state.pathSoFar.pop();
+                    return true;
+                }
+                return false;
+            },
+            childState: function childState(state, child) {
+                var cost = options.cost({ start: state.start, end: child }, state.costAvailable);
+                var nextState = _.extend({}, state, {
+                    start: child,
+                    costAvailable: state.costAvailable - cost
+                });
+
+                settings.debug.dsl && console.log('childState: '+
+                    state.start.name+' -> '+child.name+' : '+
+                    GraphAlgorithms.prototype.edgeLength({start: nextState.start, end: nextState.end})+' m, ' +
+                    cost/3600+' hrs');
+                settings.debug.dsl && console.log(' childState: cost', cost/3600, nextState.costAvailable/3600);
+                nextState.pathSoFar.push(child._id);
+                return nextState;
+            }
+        };
+
         settings.debug.trace && console.log('>pathsWithTime', graph, start, end, options);
         options = _.extend({}, defaultOptions, options || {});
         if (!options.cost) {
             options.cost = GraphAlgorithms.prototype.makeEdgeSailingTime(options.speed);
         }
-        var paths = this.dsl(graph, {
-            start: start,
-            end: end,
-            costAvailable: options.time,
-            costFn: options.cost,
-            pathSoFar: [],
-            maxEdgeRepeats: options.maxEdgeRepeats
+        var paths = this.dsl(graph, start, end, {
+            strategy: strategy,
+            state: {
+                maxEdgeRepeats: options.maxEdgeRepeats,
+                costAvailable: options.time,
+                pathSoFar: []
+            }
         });
         if (!paths) {
             paths = [];
